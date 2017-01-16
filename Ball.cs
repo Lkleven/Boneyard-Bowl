c22bf46ec1e7d279169afb7f40fbdc95095771ab
Lars Ivar Kleven <likleven@gmail.com>
2017-01-16 17:25:04 +0100

    Ball + Camera scripts
    Moving the ball
    Camera following the ball down the lane

---
+++ Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Vector3 launchVelocity;
+
+	private Rigidbody rb;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
+		Launch ();
+	}
+
+	public void Launch (){
+		rb.velocity = launchVelocity;
+		audioSource.Play ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//rb.AddForce(transform.forward * -speed);
+	
+	}
+}
---
+++ CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public Ball ball;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - ball.transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z <= 1650f) {				//z.position of the headpin is at 1879
+			transform.position = ball.transform.position + offset;
+		}
+
+
+	
+	}
+}
