fe780701b8f56adf8fa2baa02ce941561faa5ee1
Lars Ivar Kleven <likleven@gmail.com>
2017-01-24 17:37:34 +0100

    Lecture 216
    Before refactoring and architecture changes in 217

--- Ball.cs
+++ Ball.cs
@@ -8,14 +8,44 @@ public class Ball : MonoBehaviour {
 	private Rigidbody rb;
 	private AudioSource audioSource;
 	private Vector3 ballStartPosition;
+	private bool resetGutterBall = false, inPinSetterCollider = false;
+	private float gutterBallResetTime = 4f;				//Change this? Change update in Update()
+	private float timePerTurnAfterBallLaunch = 20f;		//Change this?, Change update in Reset()
+	private PinSetter pinSetter;
 
 
 	// Use this for initialization
 	void Start () {
+		pinSetter = GameObject.FindObjectOfType<PinSetter> ();
 		rb = GetComponent<Rigidbody> ();
 		rb.useGravity = false;
 		audioSource = GetComponent<AudioSource> ();
 		ballStartPosition = transform.position;
+	}
+
+	void Update(){
+
+		//Handles gutterballs
+		if (resetGutterBall) {
+			gutterBallResetTime -= Time.deltaTime;
+			if (gutterBallResetTime < 0f) {
+				//Debug.Log ("Gutterball");
+				pinSetter.PinMachine (0);				//Treated as a bowl of 0
+				Reset ();
+				gutterBallResetTime = 4f;
+				resetGutterBall = false;
+			}
+		}
+
+		//Resets ball if its too slow to know down any pins or leave the play area within a timeframe
+		if (inPlay) {
+			timePerTurnAfterBallLaunch -= Time.deltaTime;
+			if (timePerTurnAfterBallLaunch < 0f){
+				//Debug.Log ("Ball TimeOut");
+				Reset ();
+				pinSetter.PinMachine (0);					//Treated as a bowl of 0
+			}
+		}
 	}
 
 	public void Launch (Vector3 velocity){
@@ -29,20 +59,31 @@ public class Ball : MonoBehaviour {
 		if(collider.name.Equals("LaneCollider")){
 			audioSource.Play();
 		}
+		if(collider.name.Equals("PinSetter")){
+			inPinSetterCollider = true;
+		}
+
 	}
 
 	void OnTriggerExit(Collider collider){
 		if(collider.name.Equals("LaneCollider")){
 			audioSource.Stop();
 		}
+		//Ball leaves the playarea before it hits the pin area box
+		if(collider.name.Equals("PlayAreaCollider") && !inPinSetterCollider){
+			resetGutterBall = true;
+		}
 	}
 
 	public void Reset(){
 		inPlay = false;
+		inPinSetterCollider = false;
+		timePerTurnAfterBallLaunch = 20f;			//resets the launchBallResetTimer
 		rb.useGravity = false;
 		rb.velocity = Vector3.zero;
 		rb.angularVelocity = Vector3.zero;
 		transform.position = ballStartPosition;
+		transform.rotation = Quaternion.identity;
 		//cameraControl.ResetCameraPosition ();
 	}
 }
--- PinSetter.cs
+++ PinSetter.cs
@@ -5,18 +5,21 @@ using System.Collections;
 public class PinSetter : MonoBehaviour {
 	public Text pinCount;
 	public GameObject pinWheel,pinSet;
-	public int lastStandingCount = -1;
 
-	private Ball ball;
+	private int lastStandingCount = -1;
 	private bool ballEnteredBox = false;
 	private float lastChangeTime;
 	private int lastSettledCount = 10;
-	private ActionMaster actionMaster;
+	private ActionMaster actionMaster = new ActionMaster();
+
+	private Ball ball;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		pinCount.text = CountStanding().ToString ();
 		ball = GameObject.FindObjectOfType<Ball> ();
+		animator = GetComponent<Animator> ();
+		pinCount.text = CountStanding().ToString ();
 	}
 	
 	// Update is called once per frame
@@ -45,15 +48,26 @@ public class PinSetter : MonoBehaviour {
 	}
 
 	void PinsHaveSettled(){
-		int pinCount = lastSettledCount - CountStanding ();	//pinCount counts standing pins for scoring
-		lastSettledCount = CountStanding ();				//Updates lastSettledCount to current standing pins
-
+		int standing = CountStanding ();
+		int pinFall = lastSettledCount - standing;	//pinFall stores the amount standing pins for scoring
+		lastSettledCount = standing;				//Updates lastSettledCount to current standing pins
+		PinMachine(pinFall);
 		ball.Reset();
 		lastStandingCount = -1; //Indicates pins have settled, and ball not back in box
 		ballEnteredBox = false;
 	}
 
-	void PinsFallen(){
+	public void PinMachine(int pins){
+		ActionMaster.Action action = actionMaster.Bowl (pins);
+		Debug.Log ("pins down: " + pins + " - " + action);
+
+		if (action == ActionMaster.Action.Tidy) { 
+			animator.SetTrigger ("tidyTrigger");
+		}
+		else if (action == ActionMaster.Action.Reset || action == ActionMaster.Action.EndTurn || action == ActionMaster.Action.EndGame){ 
+			animator.SetTrigger("resetTrigger");
+			lastSettledCount = 10;		//Resets to 10 pins standing
+		}
 		
 	}
 
@@ -74,13 +88,17 @@ public class PinSetter : MonoBehaviour {
 		if (thingLeft.GetComponent<Pin> ()) {
 			Destroy (thingLeft);
 		}
+		if (thingLeft.GetComponent<Ball> ()) {
+			ballEnteredBox = true;
+		}
+
 	}
 
-	void OnTriggerEnter(Collider collider){
+	/*void OnTriggerEnter(Collider collider){
 		if (collider.GetComponent<Ball> ()) {
 			ballEnteredBox = true;
 		}
-	}
+	}*/
 
 	public void RaisePins(){
 		foreach (Pin pin in GameObject.FindObjectsOfType<Pin>()) {
@@ -98,6 +116,7 @@ public class PinSetter : MonoBehaviour {
 		RemoveEmptyPinContainers ();
 		GameObject newPins = Instantiate (pinSet);
 		newPins.transform.position += new Vector3 (0, 40, 0);	//instantiate at the same height as distanceToRaise in Pin.cs due to lowering them again instantly
+		pinCount.text = "10";									// resets UI counter at 10 pins
 	}
 
 	private void RemoveEmptyPinContainers(){
